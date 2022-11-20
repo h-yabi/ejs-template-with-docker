ad55de75b5b62ac7157f452f14f7f91df78a22c1
h_yabiku <>
2022-11-20 20:30:46 +0900

    update: css setting

--- postcss.config.js
+++ postcss.config.js
@@ -1,9 +1,9 @@
 module.exports = {
     plugins: [
-        require('autoprefixer')({
-            browsers: ['last 2 versions'],
-            cascade: false
-        }),
+        // require('autoprefixer')({
+        //     browsers: ['last 2 versions'],
+        //     cascade: false
+        // }),
         require('cssnano')({
             preset: 'default',
         })
